fe8c8cc6aa7fd73f49d3846aa07ccc7504570a1b
soksreng <csoksreng@gmail.com>
2025-02-03 22:39:46 -0600

    Restrict dashboard access to admins only

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,5 +1,6 @@
 <?php
 include 'partial/header.php';
+session_start();
 ?>
 
 <section class="dashboard">
@@ -15,10 +16,12 @@ include 'partial/header.php';
             <ul>
                 <li><a href="add-post.php"><i class="fa-solid fa-pen-nib"></i><h5>Add Post</h5></a></li>
                 <li><a href="dashboard.php" class ="active"><i class="fa-solid fa-pen-to-square"></i><h5>Manage Post</h5></a></li>
-                <li><a href="add-user.php"><i class="fa-solid fa-user"></i><h5>Add User</h5></a></li>
-                <li><a href="manage-users.php" ><i class="fa-solid fa-user-pen"></i><h5>Manage User</h5></a></li>
-                <li><a href="add-category.php"><i class="fa-regular fa-pen-to-square"></i><h5>Add Category</h5></a></li>
-                <li><a href="manage-categories.php" ><i class="fa-solid fa-list"></i><h5>Manage Categories</h5></a></li>
+                <?php if (isset($_SESSION['is_admin'])) : ?>
+                    <li><a href="add-user.php"><i class="fa-solid fa-user"></i><h5>Add User</h5></a></li>
+                    <li><a href="manage-users.php" ><i class="fa-solid fa-user-pen"></i><h5>Manage User</h5></a></li>
+                    <li><a href="add-category.php"><i class="fa-regular fa-pen-to-square"></i><h5>Add Category</h5></a></li>
+                    <li><a href="manage-categories.php" ><i class="fa-solid fa-list"></i><h5>Manage Categories</h5></a></li>
+                <?php endif;?>
             </ul>
         </aside>
 
