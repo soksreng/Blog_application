941ae21a811394bc55d0abaad88cf2b026ea28d9
soksreng <csoksreng@gmail.com>
2025-02-05 14:12:10 -0600

    Auto-fill first and last name in edit user form

---
+++ admin/edit-user-data.php
@@ -0,0 +1,11 @@
+<?php 
+session_start();
+// update the database
+
+if(isset($_POST['submit'])){
+    
+
+}
+
+
+?>
--- admin/edit-user.php
+++ admin/edit-user.php
@@ -1,20 +1,32 @@
 <?php
 include 'partial/header.php';
+session_start();
+
+//fetch user id from the database by using get
+if(isset($_GET['id'])){
+    $user_id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+    $sql = "SELECT * FROM users WHERE id = $user_id";
+    $result = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_assoc($result);
+} else {
+    header('Location: manage-users.php');
+    die();
+}
 ?>
 <section class="form_section">
     <div class="container form_section-container">
         <h2>Edit User</h2>
 
-        <form action="">
-            <input type="text" placeholder="First Name">
-            <input type="text" placeholder="Last Name">
+        <form action="add-user-data.php" method="POST">
+            <input type="text" value="<?= $user['first_name']?>"name="firstname"placeholder="First Name">
+            <input type="text" value="<?= $user['last_name']?>" name= "lastname"placeholder="Last Name">
             <label for="user_role">User Role</label>
-            <select >
+            <select name= "role" id="role">
                 <option value="0">Author</option>
                 <option value="1">Admin</option>
             </select>
             
-            <button type="submit" class="btn">Update User</button>
+            <button type="submit" name="submit" class="btn">Update User</button>
                 
             
         </form>
